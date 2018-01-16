879d39f0804596655f97f1c7c061fa0eedfbab1c
Yuan Gao <gaoyuanbj@yahoo.com>
2018-01-15 22:28:19 -0500

    cards

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -12,7 +12,6 @@ defmodule Cards do
       :world
 
   """
-
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
@@ -40,5 +39,29 @@ defmodule Cards do
 
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    #{status, binary} = File.read(filename)
+
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "That file does not exist"
+    end
+  end
+
+  def create_hand(hand_size) do
+    #deck = Cards.create_deck
+    #deck = Cards.shuffle(deck)
+    #hand = Cards.deal(deck, hand_size)
+
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
   end
 end
