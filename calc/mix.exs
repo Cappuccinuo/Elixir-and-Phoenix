602879eb6d462d6e8fe9492f3d5733a639fc592c
Yuan Gao <gaoyuanbj@yahoo.com>
2018-02-04 17:05:03 -0500

    Publish the package

--- calc/mix.exs
+++ calc/mix.exs
@@ -7,7 +7,9 @@ defmodule Calc.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "Calculator",
+      package: package()
     ]
   end
 
@@ -21,10 +23,17 @@ defmodule Calc.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.12"}
-      # {:calc, "~> 0.1.0"}
+      {:ex_doc, "~> 0.12"},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: [" Yuan "],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/Cappuccinuo/Elixir-and-Phoenix/tree/master/calc"}
     ]
   end
 end
