09e17014310498ad676c1511239e759c16509fea
Yuan Gao <gaoyuanbj@yahoo.com>
2018-02-12 19:07:38 -0500

    microblog

---
+++ microblog/lib/microblog/social/post.ex
@@ -0,0 +1,20 @@
+defmodule Microblog.Social.Post do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias Microblog.Social.Post
+
+
+  schema "posts" do
+    field :body, :string
+    field :user_id, :id
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(%Post{} = post, attrs) do
+    post
+    |> cast(attrs, [:body])
+    |> validate_required([:body])
+  end
+end
